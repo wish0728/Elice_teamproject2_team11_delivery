0ba90c5254748aa6754e816fea019cb3aba43c46
HyeongSeoku <gudtjr3437@naver.com>
2021-12-23 22:23:01 +0900

    feat : id와 pwd 길이 제한 조건 추가

--- front-end/src/Components/Modal.jsx
+++ front-end/src/Components/Modal.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { ID_MAX_LEN, ID_MIN_LEN, PWD_MIN_LEN } from "../constants/standard";
 
 const ModalContainer = styled.div`
   width: 100%;
@@ -21,6 +22,7 @@ const ModalContents = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  border-radius: 8px;
   background-color: black;
   color: white;
 `;
@@ -53,10 +55,19 @@ const ModalPwd = styled.input.attrs({
   margin-bottom: 20px;
 `;
 
-const ModalCreate = styled.button`
+const ModalSubmit = styled.button`
   width: 200px;
   height: 30px;
   border-radius: 8px;
+  color: black;
+  margin-bottom: 15px;
+`;
+
+const ModalCreate = styled.button`
+  width: 150px;
+  height: 30px;
+  border-radius: 8px;
+  background-color: rgba(100, 180, 243);
   color: black;
   margin-bottom: 15px;
 `;
@@ -74,14 +85,45 @@ const StyledLink = styled(Link)`
 `;
 
 const Modal = () => {
-  //아이디, 비밀번호에 대한 상태관리 작성 필요
+  //아이디, 비밀번호에 대한 상태관리 작성 필요 (recoil)
 
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const onIdChange = (e) => {
+    setId(e.target.value);
+    if (e.target.value.length > ID_MAX_LEN) {
+      alert("아이디는 15자리를 초과할수 없습니다!");
+      setId(e.target.value.slice(0, ID_MAX_LEN));
+    }
+  };
+
+  const onPwdChange = (e) => {
+    setPwd(e.target.value);
+  };
+
+  const onSubmit = () => {
+    //id 최소길이 충족 못할시
+    if (id.length < ID_MIN_LEN) {
+      alert("아이디 길이가 너무 짧습니다. (최소 5자리)");
+      //아이디 input 포커스
+    } else if (pwd.length < PWD_MIN_LEN) {
+      alert("비밀 번호 길이가 너무 짧습니다 (최소 8자리)");
+    } else {
+      //유저 데이터 넘겨줄 로직 작성 예정
+      console.log(id, pwd);
+      //넘겨준 뒤 초기화
+      setId("");
+      setPwd("");
+    }
+  };
   return (
     <ModalContainer>
       <ModalContents>
         <ModalTitle>LOGIN</ModalTitle>
-        <ModalEmail placeholder="email" />
-        <ModalPwd placeholder="password" />
+        <ModalEmail placeholder="email" value={id} onChange={onIdChange} />
+        <ModalPwd placeholder="password" value={pwd} onChange={onPwdChange} />
+        <ModalSubmit onClick={onSubmit}>ok</ModalSubmit>
         <ModalCreate>create account</ModalCreate>
         <StyledLink to="/usersetting">forgot?</StyledLink>
       </ModalContents>
