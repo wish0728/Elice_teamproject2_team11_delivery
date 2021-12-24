bb366998ca0d0869988ddb4db153c9173c471ff9
Yun Hee <dstdbsgml@naver.com>
2021-12-24 09:16:24 +0900

    chore: docker-compose 코드 실시간 반영

--- back-end/app.py
+++ back-end/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def test():
-    return 'test 성공 디버깅 시도 '
+    return 'test 성공 디버깅 세번째 시도 ddddddddd'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
