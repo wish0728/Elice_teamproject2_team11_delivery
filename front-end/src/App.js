a023ba28db1182241d40cbe820ed33dc0a90d4be
HyeongSeoku <gudtjr3437@naver.com>
2021-12-24 22:51:51 +0900

    feat : 경로변경 & 새로운 페이지 추가

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Modal from "./Components/Modal.jsx";
 import AuthSetting from "./Pages/AuthSetting";
+import Mytown from "./Pages/Mytown";
+import Othertown from "./Pages/Othertown";
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<Modal />} />
+      <Route exact path="/" element={<Home />} />
       <Route exact path="usersetting" element={<AuthSetting />} />
+      <Route exact path="/mytown" element={<Mytown />} />
+      <Route exact path="/othertown" element={<Othertown />} />
       {/* 경로 임시 설정 추후 변경 */}
     </Routes>
   );
