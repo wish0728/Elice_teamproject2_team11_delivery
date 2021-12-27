57453688c53889ac4e6921536a43e189fe756e92
Yun Hee <dstdbsgml@naver.com>
2021-12-27 14:44:20 +0900

    merge전 commit
    Former-commit-id: 0d9e273ab1351cdd3df1c13423d3ec0c10f3751b

--- back-end/app.py
+++ back-end/app.py
@@ -1,10 +1,26 @@
 from flask  import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api, Resource, reqparse
+# from flask_cors import CORS #comment this on deployment
 
-app = Flask(__name__)
+import config
 
-@app.route('/')
-def test():
-    return 'test 성공 디버깅 세번째 시도 ddddddddd'
+db = SQLAlchemy()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
+
+    app.config.from_object(config) # config 에서 가져온 파일을 사용합니다.
+    
+    db.init_app(app) # SQLAlchemy 객체를 app 객체와 이어줍니다.
+    Migrate().init_app(app, db)
+
+    app.secret_key = "secret"
+    app.config['SESSION_TYPE'] = 'filesystem'
+    
+    return app
+
+if __name__ == "__main__":
+    create_app().run(host='0.0.0.0', debug=True, port=3333)
