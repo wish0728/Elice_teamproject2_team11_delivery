6a9d1748ce10991da15cacebedf0b9524a93550c
HyeongSeoku <gudtjr3437@naver.com>
2021-12-27 13:40:38 +0900

    버튼 상태 컨트롤 하기 위한 atom 추가

--- front-end/src/state.js
+++ front-end/src/state.js
@@ -5,4 +5,24 @@ const loginState = atom({
   default: false,
 });
 
-export { loginState };
+const firstLocationState = atom({
+  key: "first-location",
+  default: false,
+});
+
+const secondLocationState = atom({
+  key: "second-location",
+  default: false,
+});
+
+const thridLocationState = atom({
+  key: "thrid-location",
+  default: false,
+});
+
+export {
+  loginState,
+  firstLocationState,
+  secondLocationState,
+  thridLocationState,
+};
