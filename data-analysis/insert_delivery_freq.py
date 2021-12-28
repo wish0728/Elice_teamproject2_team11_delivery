4f99cc1b773c099cabf787630ad56d0c5fa975a5
[3기 2반]-박다혜 <thvldk40@naver.com>
2021-12-28 17:37:23 +0900

    user table 추가 insert_delivery_freq 내부 경로 수정
    Former-commit-id: fdb8c08428b40ea156a72728fdd423d04f47e23f

--- data-analysis/insert_delivery_freq.py
+++ data-analysis/insert_delivery_freq.py
@@ -10,14 +10,15 @@ connection = sqlite3.connect('../NaplessRabbit.db')
 cursor = connection.cursor()
 
 # Opening the Books_info.csv file
-df_2019=pd.read_csv("../rawdata/elicer_2019_col_name.csv")
-df_2020_1=pd.read_csv("../rawdata/elicer_2020_1_col_name.csv", encoding = 'cp949')
-df_2020_2=pd.read_csv("../rawdata/elicer_2020_2_col_name.csv")
-df_2021=pd.read_csv("../rawdata/elicer_2021_col_name.csv", encoding = 'cp949')
+df_2019=pd.read_csv("../rawdata/시간-지역별배달주문건수/elicer_2019_col_name.csv")
+df_2020_1=pd.read_csv("../rawdata/시간-지역별배달주문건수/elicer_2020_1_col_name.csv")
+df_2020_2=pd.read_csv("../rawdata/시간-지역별배달주문건수/elicer_2020_2_col_name.csv")
+df_2021=pd.read_csv("../rawdata/시간-지역별배달주문건수/elicer_2021_col_name.csv")
 
 df = pd.concat([df_2019, df_2020_1, df_2020_2, df_2021], ignore_index=True)
-
+# print(df.tail())
 for row in df.itertuples():
+	# print(row.날짜, row.시간대, row.배달건수, row.광역시도, row.시군구, row.읍면동)
 	cursor.execute('''INSERT INTO deliveryfreq_by_time_area (date, time, delivery_freq
 	, area1_City_Do, area2_Si_Gun_Gu, area3_Dong) VALUES(?, ?, ?, ?, ?, ?)''', 
 	[row.날짜, row.시간대, row.배달건수, row.광역시도, row.시군구, row.읍면동])
