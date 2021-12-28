7459446cb4a44a1e3846d91a9e1d519871bdef16
Yun Hee <dstdbsgml@naver.com>
2021-12-28 17:46:41 +0900

    feat: rest-api 시간대별 평균 주문량
    Former-commit-id: 64f7e84113e1770b239c272aaa9a0e53f93a1113

--- back-end/models.py
+++ back-end/models.py
@@ -18,11 +18,17 @@ class weather(db.Model):
     __tablename__ = "weather"
 
     id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-    date = db.Column(db.Date, db.ForeignKey('deliveryfreq_by_time_area.date'), nullable=False)
-    time = db.Column(db.Integer, db.ForeignKey('deliveryfreq_by_time_area.time'), nullable=False)
-    area1_City_Do = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area1_City_Do'), nullable=False)
-    area2_Si_Gun_Gu = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area2_Si_Gun_Gu'), nullable=False)
-    area3_Dong = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area3_Dong'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Integer, nullable=False)
+    area1_City_Do = db.Column(db.String(45), nullable=False)
+    area2_Si_Gun_Gu = db.Column(db.String(45), nullable=False)
+    area3_Dong = db.Column(db.String(45), nullable=False)
+
+    # date = db.Column(db.Date, db.ForeignKey('deliveryfreq_by_time_area.date'), nullable=False)
+    # time = db.Column(db.Integer, db.ForeignKey('deliveryfreq_by_time_area.time'), nullable=False)
+    # area1_City_Do = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area1_City_Do'), nullable=False)
+    # area2_Si_Gun_Gu = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area2_Si_Gun_Gu'), nullable=False)
+    # area3_Dong = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area3_Dong'), nullable=False)
 
     temperature = db.Column(db.Integer, nullable=True)
     rain = db.Column(db.Float)
@@ -30,21 +36,26 @@ class weather(db.Model):
     dust = db.Column(db.Float)
     weather_alert = db.Column(db.String(45))
 
-    deliveryfreq_by_time_area = relationship('deliveryfreq_by_time_area')
+    
 
 class Corona_info(db.Model):
     __tablename__ = 'Corona_info'
 
     id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-    date = db.Column(db.Date, db.ForeignKey('deliveryfreq_by_time_area.date'), nullable=False)
-    area1_City_Do = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area1_City_Do'), nullable=False)
-    area2_Si_Gun_Gu = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area2_Si_Gun_Gu'))
-    area3_Dong = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area3_Dong'))
+    date = db.Column(db.Date, nullable=False)
+    area1_City_Do = db.Column(db.String(45), nullable=False)
+    area2_Si_Gun_Gu = db.Column(db.String(45))
+    area3_Dong = db.Column(db.String(45))
+
+    # date = db.Column(db.Date, db.ForeignKey('deliveryfreq_by_time_area.date'), nullable=False)
+    # area1_City_Do = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area1_City_Do'), nullable=False)
+    # area2_Si_Gun_Gu = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area2_Si_Gun_Gu'))
+    # area3_Dong = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area3_Dong'))
 
     distancing_level = db.Column(db.Integer)
     restrict_time = db.Column(db.Integer)
     restrict_headcount = db.Column(db.Integer)
 
-    deliveryfreq_by_time_area = relationship('deliveryfreq_by_time_area')
+    
 
 
