a74906c5de58baad3fac8f31cce12498c915eedd
Yun Hee <dstdbsgml@naver.com>
2021-12-29 16:38:08 +0900

    feat: 시간대별 평균 배달건수(2019~2021) 통합
    Former-commit-id: 84e5f273b5a0ff0398e1bf0c8274370d963f9293

--- back-end/apis/delivery.py
+++ back-end/apis/delivery.py
@@ -45,18 +45,40 @@ class getArea2(Resource):
 # 시군구 입력 -> 시간대별 평균 
 # area1과 area2에 따라 0~23에 해당하는 배달건수 평균값 보내주기 
 # 시군구 전체 데이터를 보고 싶으면 area2에 '전체'를 보내주기 
-@deliveryfreq.route('/getFreq/<int:year>/<string:area1>/<string:area2>')
+@deliveryfreq.route('/getFreq/<string:area1>/<string:area2>')
+@deliveryfreq.response(200, "Found")
+@deliveryfreq.response(404, "Not found")
+@deliveryfreq.response(500, "Internal Error")
+class getFreq(Resource): 
+    def get(self,area1,area2):
+        '''해당 연도와 시군구와 일치하는 시간대별 배달건수 평균을 가져옵니다.'''
+        if area2=='전체':
+            rows = d.query.filter_by(area1_City_Do=area1).all()   
+        else:
+            rows = d.query.filter_by(area1_City_Do=area1,area2_Si_Gun_Gu=area2).all()     
+        data={}
+        a={}
+        for row in rows:
+            time = int(row.time)
+            freq = int(row.delivery_freq)
+            try:
+                data[time] += freq
+                a[time] += 1
+            except KeyError:
+                data[time] = freq
+                a[time] = 1
+        for key in data.keys():
+            data[key] /= a[key]
+        return jsonify(json_list=data)
+
+
+@deliveryfreq.route('/getFreqByYear/<int:year>/<string:area1>/<string:area2>')
 @deliveryfreq.response(200, "Found")
 @deliveryfreq.response(404, "Not found")
 @deliveryfreq.response(500, "Internal Error")
 class getFreq(Resource): 
     def get(self,year,area1,area2):
         '''해당 연도와 시군구와 일치하는 시간대별 배달건수 평균을 가져옵니다.'''
-        query = d.query.filter_by(area1_City_Do=area2)
-        df = pd.read_sql(query.statement, query.session.bind)
-        df = df.groupby('time').mean()
-        print(df)
-
         start = date(year=year,month=1,day=1)
         end = date(year=year,month=12,day=31)
         if area2=='전체':
@@ -104,6 +126,7 @@ class getSum(Resource):
         return jsonify(json_list=data)
 
 
+# 보류 
 # 시간대별 평균 배달건수 top 3 (2019,2020,2021 다 주기)
 # 시군구 전체 데이터를 보고 싶으면 area2에 '전체'를 보내주기
 # 점심:0, 저녁:1, 야식:2 - 나중에 회의 후 수정 
@@ -132,26 +155,7 @@ class yearTop3(Resource):
         #     print(df.groupby('area3_Dong')['delivery_freq'].mean().sort_values(by='delivery_freq',ascending=False).reset_index().head(3))
         return jsonify(json_list=test.date.year)
 
-# 슬아님 방법 적용한 평균(func.avg) - 일단 실패 
-# @deliveryfreq.route('/getFreqTest/<string:name>')
-# @deliveryfreq.response(200, "Found")
-# @deliveryfreq.response(404, "Not found")
-# @deliveryfreq.response(500, "Internal Error")
-# class getFreq(Resource): 
-#     def get(self,name):
-#         # rows = db.session.query(deliveryfreq_by_time_area.time,deliveryfreq_by_time_area.delivery_freq).filter(deliveryfreq_by_time_area.area2_Si_Gun_Gu==name).group_by(deliveryfreq_by_time_area.time).all()
-#         rows = db.session.query(d.time,func.avg(d.delivery_freq)).filter(d.area2_Si_Gun_Gu==name).all()
-#         data={}
-#         name={}
-#         for row in rows:
-#             time = row[0]
-#             freq = int(row[1])
-#             data[time] = freq
-#             # try:
-#             #     data[time] += freq
-#             # except KeyError:
-#             #     data[time] = freq
-#         return jsonify(json_list=data)
+
 
 
 
