9ad4df1766f085f2cfa2c93d8d7a6b76b636c53e
CHC <whckdgma96@gmail.com>
2021-12-30 11:05:50 +0900

    로그인 기능
    Former-commit-id: 2640179c711fb2c903b8126d1542b4f6f779f7a0

--- back-end/app.py
+++ back-end/app.py
@@ -4,6 +4,9 @@ from flask_migrate import Migrate
 from flask_restx import Api, apidoc, Resource, reqparse
 from flask_cors import CORS
 
+# import models
+# from apis.delivery import deliveryfreq
+# from apis.auth import Auth
 
 import config
 
@@ -11,7 +14,9 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    
+    import models
+    from apis.delivery import deliveryfreq
+    from apis.auth import Auth
     app.config.from_object(config) # config 에서 가져온 파일을 사용합니다.
     
     # app.secret_key = "secret"
@@ -21,10 +26,10 @@ def create_app():
 
     db.init_app(app) # SQLAlchemy 객체를 app 객체와 이어줍니다.
     Migrate().init_app(app, db)
-    import models
-    from apis.delivery import deliveryfreq
+
     api = Api(app)
     api.add_namespace(deliveryfreq)
+    api.add_namespace(Auth,'/auth')
 
     return app
 
