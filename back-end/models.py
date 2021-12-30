56aa150d6dd8a633ed9823887844512b68123045
Yun Hee <dstdbsgml@naver.com>
2021-12-30 11:41:07 +0900

    test용 커밋
    Former-commit-id: 6e5b0dad7e4bb3853f223418ecfb3ec2fc9e101f

--- back-end/models.py
+++ back-end/models.py
@@ -13,12 +13,22 @@ class deliveryfreq_by_time_area(db.Model):
     area2_Si_Gun_Gu = db.Column(db.String(45), nullable=False)
     area3_Dong = db.Column(db.String(45), nullable=False)
 
+<<<<<<< Updated upstream
 class member(db.Model):
     __tablename__ = "member"
 
     id = db.Column(db.String(10), primary_key=True, nullable=False)
     pw = db.Column(db.String(10), nullable=False)
     name = db.Column(db.String(10), nullable=False)
+=======
+class deliveryfreq_avg_by_time_area2 (db.Model):
+    __tablename__ = "deliveryfreq_avg_by_time_area2"
+
+    area1= db.Column(db.String(45), nullable=False)
+    area2= db.Column(db.String(45), nullable=False)
+    time = db.Column(db.Integer, nullable=False)
+    freqavg = db.Column(db.Integer, nullable=False)
+>>>>>>> Stashed changes
 
 class weather(db.Model):
     __tablename__ = "weather"
@@ -29,12 +39,6 @@ class weather(db.Model):
     area1_City_Do = db.Column(db.String(45), nullable=False)
     area2_Si_Gun_Gu = db.Column(db.String(45), nullable=False)
     area3_Dong = db.Column(db.String(45), nullable=False)
-
-    # date = db.Column(db.Date, db.ForeignKey('deliveryfreq_by_time_area.date'), nullable=False)
-    # time = db.Column(db.Integer, db.ForeignKey('deliveryfreq_by_time_area.time'), nullable=False)
-    # area1_City_Do = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area1_City_Do'), nullable=False)
-    # area2_Si_Gun_Gu = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area2_Si_Gun_Gu'), nullable=False)
-    # area3_Dong = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area3_Dong'), nullable=False)
 
     temperature = db.Column(db.Integer, nullable=True)
     rain = db.Column(db.Float)
@@ -52,11 +56,6 @@ class Corona_info(db.Model):
     area1_City_Do = db.Column(db.String(45), nullable=False)
     area2_Si_Gun_Gu = db.Column(db.String(45))
     area3_Dong = db.Column(db.String(45))
-
-    # date = db.Column(db.Date, db.ForeignKey('deliveryfreq_by_time_area.date'), nullable=False)
-    # area1_City_Do = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area1_City_Do'), nullable=False)
-    # area2_Si_Gun_Gu = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area2_Si_Gun_Gu'))
-    # area3_Dong = db.Column(db.String(45), db.ForeignKey('deliveryfreq_by_time_area.area3_Dong'))
 
     distancing_level = db.Column(db.Integer)
     restrict_time = db.Column(db.Integer)
