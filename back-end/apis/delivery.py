c3797620e6646dde5788802ca7c492dd237c62b8
Yun Hee <dstdbsgml@naver.com>
2021-12-30 16:13:20 +0900

    feat: area 예외 처리, db화 완료
    Former-commit-id: 7f32bcca65e19946a6eb789aca2da4a6a80b55a0

--- back-end/apis/delivery.py
+++ back-end/apis/delivery.py
@@ -3,6 +3,9 @@ from flask_restx import Resource, Namespace
 from sqlalchemy.orm import query
 from models import deliveryfreq_by_time_area as d
 from models import freqavg as fa
+from models import freqavg_by_area1 as fa1
+from models import area1_for_exception as a1
+from models import area2_for_exception as a2
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 import numpy as np
@@ -12,57 +15,31 @@ from datetime import date
 db = SQLAlchemy()
 deliveryfreq = Namespace("delivery", description="deliveryfreq_by_time_area")
 
-# area1_City_Do
-
-
-@deliveryfreq.route('/getArea1')
-@deliveryfreq.response(200, "Found")
-@deliveryfreq.response(404, "Not found")
-@deliveryfreq.response(500, "Internal Error")
-class getArea1(Resource):
-
-    '''광역시도 데이터를 가져옵니다.'''
-
-    def get(self):
-        rows = db.session.query(d.area1_City_Do).all()
-        data = []
-        for row in rows:
-            data.append(row)
-        data = list(np.unique(data))
-        return jsonify(json_list=data)
-
-# area2_Si_Gun_Gu
-
-
-@deliveryfreq.route('/getArea2/<string:area1>')
-@deliveryfreq.response(200, "Found")
-@deliveryfreq.response(404, "Not found")
-@deliveryfreq.response(500, "Internal Error")
-class getArea2(Resource):
-    def get(self, area1):
-        '''해당 광역시도에 해당하는 시군구 데이터를 가져옵니다.'''
-        rows = db.session.query(d.area2_Si_Gun_Gu).filter(
-            d.area1_City_Do == area1).all()
-        data = []
-        for row in rows:
-            data.append(row)
-        data = list(np.unique(data))
-        return jsonify(json_list=data)
 
 # 시군구 입력 -> 시간대별 평균
 # area1과 area2에 따라 0~23에 해당하는 배달건수 평균값 보내주기
 # 시군구 전체 데이터를 보고 싶으면 area2에 '전체'를 보내주기
 
+def exceptionForArea(area1,area2):
+    area1_list = [row.area1 for row in a1.query.all()]
+    area2_list = [row.area2 for row in a2.query.filter_by(area1=area1).all()]
+    area2_list += ["전체"]
+    print(area2_list)
+    if area1 not in area1_list:
+        return {"message":"Unavailable area1"}, 400
+    elif area2 not in area2_list:
+        return {"message":"Unavailable area2"}, 400
+    return 
 
 @deliveryfreq.route('/getFreq/<string:area1>/<string:area2>')
-@deliveryfreq.response(200, "Found")
-@deliveryfreq.response(404, "Not found")
-@deliveryfreq.response(500, "Internal Error")
 class getFreq(Resource):
     def get(self, area1, area2):
-        '''해당 연도와 시군구와 일치하는 시간대별 배달건수 평균을 가져옵니다.'''
+        '''해당 연도와 시군구와 일치하는 시간대별 배달건수 평균을 가져옵니다.''' 
+        if exceptionForArea(area1,area2):
+            return exceptionForArea(area1,area2)
+
         if area2 == '전체':
-            rows = d.query.filter_by(area1_City_Do=area1).all()
+            rows = fa1.query.filter_by(area1=area1).all()
         else:
             rows = fa.query.filter_by(
                 area1=area1, area2=area2).all()
@@ -81,6 +58,9 @@ class getFreq(Resource):
 class getFreqByYear(Resource): 
     def get(self,year,area1,area2):
         '''해당 연도와 시군구와 일치하는 시간대별 배달건수 평균을 가져옵니다.'''
+        if exceptionForArea(area1,area2):
+            return exceptionForArea(area1,area2)
+
         start = date(year=year, month=1, day=1)
         end = date(year=year, month=12, day=31)
         if area2 == '전체':
@@ -114,6 +94,9 @@ class getFreqByYear(Resource):
 @deliveryfreq.response(500, "Internal Error")
 class getSum(Resource):
     def get(self, year, area1, area2):
+        if exceptionForArea(area1,area2):
+            return exceptionForArea(area1,area2)
+            
         '''해당 연도와 시군구와 일치하는 시간대별 배달건수 총합을 가져옵니다.'''
         start = date(year=year, month=1, day=1)
         end = date(year=year, month=12, day=31)
@@ -139,26 +122,26 @@ class getSum(Resource):
 # 시군구 전체 데이터를 보고 싶으면 area2에 '전체'를 보내주기
 # 점심:0, 저녁:1, 야식:2 - 나중에 회의 후 수정
 
-@deliveryfreq.route('/yearTop3/<string:area1>/<string:area2>/<int:peakTime>')
-@deliveryfreq.response(200, "Found")
-@deliveryfreq.response(404, "Not found")
-@deliveryfreq.response(500, "Internal Error")
-class yearTop3(Resource):
-    def get(self, area1, area2, peakTime):
-        '''2019~2021년에 해당하는 시간대별 top3 동을 가져옵니다. '''
-        data = {}
-        test = d.query.first()
-        print(type(test.date.year))
-        # years = [2019,2020,2021]
-        # peakTime_list = [(11,13), (17,20), (21,23)] # 점심(11~13) or 저녁(17~20) or 야식(21~23)
-        # s = peakTime_list[peakTime][0]
-        # e = peakTime_list[peakTime][1]
-        # for year in years:
-        #     if area2=='전체':
-        #         query  = .filter(d.date.year==year,d.time>=s,d.time<=e).filter_by(area1_City_Do=area1)
-        #     else:
-        #         query  = d.query.filter(d.date.year==year,d.time>=s,d.time<=e).filter_by(area1_City_Do=area1,area2_Si_Gun_Gu=area2)
-        #     df = pd.read_sql(query.statement, query.session.bind)
-        #     print(f'---{year}---')
-        #     print(df.groupby('area3_Dong')['delivery_freq'].mean().sort_values(by='delivery_freq',ascending=False).reset_index().head(3))
-        return jsonify(json_list=test.date.year)
+# @deliveryfreq.route('/yearTop3/<string:area1>/<string:area2>/<int:peakTime>')
+# @deliveryfreq.response(200, "Found")
+# @deliveryfreq.response(404, "Not found")
+# @deliveryfreq.response(500, "Internal Error")
+# class yearTop3(Resource):
+#     def get(self, area1, area2, peakTime):
+#         '''2019~2021년에 해당하는 시간대별 top3 동을 가져옵니다. '''
+#         data = {}
+#         test = d.query.first()
+#         print(type(test.date.year))
+#         # years = [2019,2020,2021]
+#         # peakTime_list = [(11,13), (17,20), (21,23)] # 점심(11~13) or 저녁(17~20) or 야식(21~23)
+#         # s = peakTime_list[peakTime][0]
+#         # e = peakTime_list[peakTime][1]
+#         # for year in years:
+#         #     if area2=='전체':
+#         #         query  = .filter(d.date.year==year,d.time>=s,d.time<=e).filter_by(area1_City_Do=area1)
+#         #     else:
+#         #         query  = d.query.filter(d.date.year==year,d.time>=s,d.time<=e).filter_by(area1_City_Do=area1,area2_Si_Gun_Gu=area2)
+#         #     df = pd.read_sql(query.statement, query.session.bind)
+#         #     print(f'---{year}---')
+#         #     print(df.groupby('area3_Dong')['delivery_freq'].mean().sort_values(by='delivery_freq',ascending=False).reset_index().head(3))
+#         return jsonify(json_list=test.date.year)
--- back-end/insert_area_for_exception.py
+++ back-end/insert_area_for_exception.py
@@ -11,12 +11,12 @@ conn = sqlite3.connect('NaplessRabbit.db')
 cur = conn.cursor()
 
 # Opening the Books_info.csv file
-df = pd.read_csv('freqavg.csv')
+df = pd.read_csv('area2_for_exception.csv')
 
 i=1
 for row in df.itertuples():
-    cur.execute('''INSERT INTO freqavg (id,area1,area2,time,freqAvg) VALUES(?, ?, ?, ?, ?)''', 
-	[i,row.area1, row.area2, row.time, row.freqAvg])
+    cur.execute('''INSERT INTO area2_for_exception (id,area1,area2) VALUES(?, ?, ?)''', 
+	[i,row.area1,row.area2])
     i+=1
 # select_all = "SELECT * FROM deliveryfreq_by_time_area LIMIT 5"
 # results = cur.execute(select_all).fetchall()
