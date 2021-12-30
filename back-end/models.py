fafa85b940fcaef067d3aaa2f1b3da4064254827
Yun Hee <dstdbsgml@naver.com>
2021-12-30 16:13:20 +0900

    feat: area 예외 처리, db화 완료
    Former-commit-id: 9e438f8ae69649a6c03eda4bfce725c6b59a0cff

--- back-end/models.py
+++ back-end/models.py
@@ -28,6 +28,30 @@ class freqavg (db.Model):
     time = db.Column(db.Integer, nullable=False)
     freqavg = db.Column(db.Integer, nullable=False)
 
+class freqavg_by_area1 (db.Model):
+    __tablename__ = "freqavg_by_area1"
+
+    id = db.Column(db.String(10), primary_key=True, nullable=False)
+    area1= db.Column(db.String(45), nullable=False)
+    time = db.Column(db.Integer, nullable=False)
+    freqavg = db.Column(db.Integer, nullable=False)
+
+class area1_for_exception (db.Model):
+    __tablename__ = "area1_for_exception"
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.String(10), primary_key=True, nullable=False)
+    area1= db.Column(db.String(45), nullable=False)
+
+class area2_for_exception (db.Model):
+    __tablename__ = "area2_for_exception"
+    __table_args__ = {'extend_existing': True}
+    
+    id = db.Column(db.String(10), primary_key=True, nullable=False)
+    area1= db.Column(db.String(45), nullable=False)
+    area2= db.Column(db.String(45), nullable=False)
+
+
 class weather(db.Model):
     __tablename__ = "weather"
 
