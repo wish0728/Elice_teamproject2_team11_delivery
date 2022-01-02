1c33fdec494a910e067c800533c6ee99e7c5c802
HyeongSeoku <gudtjr3437@naver.com>
2022-01-02 18:41:22 +0900

    Feat : 엔드 포인트 변경
    Former-commit-id: 29ac7397ab6c9e1452fc5babf357b99cad25aa0c

--- front-end/src/constants/standard.js
+++ front-end/src/constants/standard.js
@@ -1,5 +1,5 @@
 //로그인 관련
-export const ID_MAX_LEN = 15;
+export const ID_MAX_LEN = 20;
 export const ID_MIN_LEN = 5;
 export const PWD_MIN_LEN = 8;
 
@@ -7,3 +7,6 @@ export const PWD_MIN_LEN = 8;
 export const MENU_BTN_1 = "우리 동네 배달건수 알아보기";
 export const MENU_BTN_2 = "이웃 동네 배달건수 구경하기";
 export const MENU_BTN_3 = "숫자로 보는 배달";
+
+//api 주소
+export const API_END_POINT = "http://127.0.0.1:5000/";
