dd1cb69fa0b33d97f98b1120a1b65e2b964432b6
HyeongSeoku <gudtjr3437@naver.com>
2022-01-03 16:32:32 +0900

    Feat : 회원가입 에러 메세지 추가
    Former-commit-id: 2ecad0bec5a8653975e5d2dee8e790f94b199fb5

--- front-end/src/constants/standard.js
+++ front-end/src/constants/standard.js
@@ -10,3 +10,10 @@ export const MENU_BTN_3 = "숫자로 보는 배달";
 
 //api 주소
 export const API_END_POINT = "http://127.0.0.1:5000/";
+
+//message
+export const REGIST_ID_ERROR = "다른 아이디를 사용해보세요. 😢 ";
+export const REGIST_ID_SUCCESS = "사용 가능한 아이디 입니다.";
+export const REGIST_ID_VALID_CHECK = "사용 가능한 아이디인지 확인해주세요.";
+export const REGIST_PWD_EMPTY = "비밀번호 확인을 체크해주세요.";
+export const REGIST_PWD_ERROR = "비밀번호와 비밀번호 확인이 동일하지 않습니다.";
