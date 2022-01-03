9f69118b1612ad29a2bf80e1d2cc41f72b5d7f8c
Yun Hee <dstdbsgml@naver.com>
2022-01-03 14:07:05 +0900

    chore: resolver, service 반영
    Former-commit-id: d2355fc247717021e0676cc80e6a5b30772e0509

--- back-end/models/delivery.py
+++ back-end/models/delivery.py
@@ -58,21 +58,7 @@ class freqavg_by_day2(db.Model):
     def as_dict(self):
         return {x.name: getattr(self, x.name) for x in self.__table__.columns if x.name in ['day','freqavg']}
 
-class area1_for_exception (db.Model):
-    __tablename__ = "area1_for_exception"
-    __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.String(10), primary_key=True, nullable=False)
-    area1= db.Column(db.String(45), nullable=False)
-    
-    
-class area2_for_exception (db.Model):
-    __tablename__ = "area2_for_exception"
-    __table_args__ = {'extend_existing': True}
-    
-    id = db.Column(db.String(10), primary_key=True, nullable=False)
-    area1= db.Column(db.String(45), nullable=False)
-    area2= db.Column(db.String(45), nullable=False)
 
 # class weather(db.Model):
 #     __tablename__ = "weather"
