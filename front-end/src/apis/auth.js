9bc11dda86d40bf79e567e90d65193a56240bae8
HyeongSeoku <gudtjr3437@naver.com>
2022-01-03 16:34:20 +0900

    Feat : 회원가입 아이디 유효성 검사 api 작성
    Former-commit-id: 094081911c832f640236542873842623b7a19a7b

--- front-end/src/apis/auth.js
+++ front-end/src/apis/auth.js
@@ -12,8 +12,8 @@ const get_api = async (url) => {
       console.error("데이터가 없습니다.");
       return false;
     case 500:
-      console.error("Internal Error");
-      return false;
+      console.log("네트워크 오류");
+      return result;
     default:
       console.log("네트워크 오류 발생");
   }
@@ -38,6 +38,9 @@ const post_api = async (url, data) => {
 const authApi = {
   send_login: async (data) => {
     return await post_api(`${AUTH_END_POINT}/login`, data);
+  },
+  valid_id: async (id) => {
+    return await get_api(`${AUTH_END_POINT}/register/${id}`);
   },
 };
 
