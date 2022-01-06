0804de70e419333b93c6ad1f0d8eade820ffbb33
CHC <whckdgma96@gmail.com>
2022-01-06 19:53:53 +0900

    test
    Former-commit-id: 7be684eebb72850461de6891aed18873635bdc3d

--- back-end/app.py
+++ back-end/app.py
@@ -3,22 +3,9 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_restx import Api
 from flask_cors import CORS
-# from db_connect import db
+from db_connect import db
 
-# import config
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-import os
-
-BASE_DIR = os.path.dirname(__file__) # 폴더 구조가 달라져도, 현재 폴더를 가져와서 사용할 수 있도록 설정합니다.
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'NaplessRabbit.db')) #vscode에서 볼 수 있음
-                    #통신방식              #패스워드는 snuggle
-# SQLALCHEMY_BINDS = "mysql+pymysql://root:snuggle@127.0.0.1:3306/Library" #터미널에서 mariadb에 접속해서도 보고싶음
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+import config
 
 def create_app():
     app = Flask(__name__)
