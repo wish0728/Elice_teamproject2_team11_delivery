ea4e59fa93ee4fe5ecda8fadc947ba48b57af455
CHC <whckdgma96@gmail.com>
2022-01-07 21:39:01 +0900

    auth 파일 resolver, service 로 분할
    Former-commit-id: f9a070bb19507742d21b4818481f508e80859e3f

--- back-end/app.py
+++ back-end/app.py
@@ -13,8 +13,7 @@ def create_app():
     CORS(app)
     #일단 services로 대체 -> 후에 resolver로 하기 
     from apis.delivery.deliveryResolver import Deliveryfreq
-    from apis.auth.auth import Auth
-    # from apis.auth.authResolver import Auth
+    from apis.auth.authResolver import Auth
     from apis.map.mapResolver  import Map
     app.config.from_object(config)  # config 에서 가져온 파일을 사용합니다.
 
